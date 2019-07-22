5015a0b8c3fc1ab2dd824e2a001eb8ea56060223
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-07-22 13:44:56 +0700

    Add new examples

---
+++ examples/live-reload/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+	fp "path/filepath"
+
+	"github.com/RadhiFadlillah/qamel"
+)
+
+func main() {
+	// Create application
+	app := qamel.NewApplication(len(os.Args), os.Args)
+	app.SetApplicationDisplayName("Live Reload Example")
+
+	// Create a QML viewer
+	view := qamel.NewViewerWithSource("res/main.qml")
+	view.SetResizeMode(qamel.SizeRootObjectToView)
+	view.SetHeight(300)
+	view.SetWidth(400)
+	view.Show()
+
+	// Watch change in resource dir
+	projectDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Failed to get working directory:", err)
+	}
+
+	resDir := fp.Join(projectDir, "res")
+	go view.WatchResourceDir(resDir)
+
+	// Exec app
+	app.Exec()
+}
