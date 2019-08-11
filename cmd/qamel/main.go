34a4e6a95c90762e28dedb84fd19d626c0fbb8c7
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-08-11 19:22:07 +0700

    Add version to cmd

--- cmd/qamel/main.go
+++ cmd/qamel/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/RadhiFadlillah/qamel/internal/cmd"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	err := cmd.QamelCmd().Execute()
-	if err != nil {
-		logrus.Fatalln(err)
+	rootCmd := cmd.QamelCmd()
+	rootCmd.Version = version
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
---
+++ cmd/qamel/version.go
@@ -0,0 +1,3 @@
+package main
+
+var version = "715de5446c83d5543369f3a88891f4e170cc0d2a"
